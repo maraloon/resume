e6e4e8db4e7d102707d2c16ad9407f695a5b7a41
Artur Grigorev <zeroly@ya.ru>
2018-01-13 10:17:13 +0300

    Install dependencies Create Company Entity

---
+++ src/Entity/Company.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
+ */
+class Company
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $site;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $phoneNum;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getSite(): string
+    {
+        return $this->site;
+    }
+
+    public function setSite(string $site)
+    {
+        $this->site = $site;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address)
+    {
+        $this->address = $address;
+    }
+
+    public function getPhoneNum(): string
+    {
+        return $this->phoneNum;
+    }
+
+    public function setPhoneNum(string $phoneNum)
+    {
+        $this->phoneNum = $phoneNum;
+    }
+}
